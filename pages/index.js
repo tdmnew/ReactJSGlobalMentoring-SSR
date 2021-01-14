e0311d3672468ad44ebbe9142ad1846c0adc50c4
Tim New <tim@EPGBLONW0147.budapest.epam.com>
2021-01-14 11:09:14 +0000

    Changed landing page behaviour

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
 import Header from "../components/Header/Header.js";
 import Footer from "../components/Footer/Footer.js";
 import Nav from "../components/Nav/Nav.js";
-
+import MovieList from "../components/MovieList/MovieList.js";
 import NoMovies from "../components/NoMovies/NoMovies.js";
 
 export default function Home() {
+    const [listActive, setListActive] = React.useState(false);
+    const movies = useSelector((state) => state.movies);
+
+    React.useEffect(() => {
+        if(movies.length > 0) {
+            setListActive(true);
+        }
+    }, [movies])
+
     return (
         <>
             <Header />
             <Nav />
-            <NoMovies />
+            { listActive ? <MovieList /> : <NoMovies /> }
             <Footer />
         </>
     );
