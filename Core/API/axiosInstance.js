e770669040901b98da17866dc7bd1ea76635700f
Tim <tim@localhost.localdomain>
2021-03-24 20:17:16 +0000

    changed axiosInstance to check for prod and dev

--- Core/API/axiosInstance.js
+++ Core/API/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.DEV_URI
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.PROD_URI : process.env.DEV_URI
 
 const api = axios.create({
   baseURL,
